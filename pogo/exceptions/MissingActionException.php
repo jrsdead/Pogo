6b1d35d1c0dcf589eff3a5f553b47f75b5c78b24
jrsdead <jrsdead@gmail.com>
2011-11-03 14:55:43 +0000

    exceptions

---
+++ pogo/exceptions/MissingActionException.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * Description of MissingActionException
+ *
+ * @author jrsdead
+ */
+/*
+ * Copyright (c) 2011, jrsdead
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the <organization> nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * 
+ * PROJECT: pogo
+ * FILE: MissingActionException.php
+ * DESCRIPTION: DESCRIPTION GOES HERE
+ */
+
+namespace Pogo\Exceptions;
+
+use \Exception;
+
+class MissingActionException extends Exception
+{
+    public function __construct($missingAction) {
+	$message = "The requested action (" . $missingAction . ") was not found.";
+	parent::__construct($message);
+    }
+}
+?>
